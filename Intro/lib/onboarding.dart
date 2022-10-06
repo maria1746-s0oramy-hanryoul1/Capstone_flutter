a1e1aa2f4c709568f8e6040d51ba6a48699c9323
Soorim <soo031100@naver.com>
2022-10-07 01:07:10 +0900

    Onboarding

---
+++ Intro/lib/onboarding.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'main.dart';
+
+class OnBoardingPage extends StatelessWidget {
+  const OnBoardingPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IntroductionScreen(
+      pages: [
+        PageViewModel(
+          title: 'UMA UMA\nうまうま',
+          body: '우마우마에 오신 것을 환영합니다',
+          image: Image.asset('image/logo.png'),
+          decoration: getPageDecoration()
+        ),
+        PageViewModel(
+            title: '나의 취향을 고르다',
+            body: '무엇을 먹을지 고민된다면, \n 일식을 좋아한다면,\n 맛집 추천을 원하다면,\n 우마우마',
+            image: Image.asset('image/logo.png'),
+            decoration: getPageDecoration()
+        ),
+        PageViewModel(
+            title: '지금 바로 \n 테스트를 시작하세요',
+            body: '다음페이지로 넘어가면 시작됩니다',
+            image: Image.asset('image/logo.png'),
+            decoration: getPageDecoration()
+        ),
+      ],
+      done: const Text('done'),
+      onDone: (){
+        Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (context) => const MyPage()),
+        );
+      },
+      next: const Icon(Icons.arrow_forward),
+      showSkipButton: true,
+      skip: const Text('skip'),
+      dotsDecorator: DotsDecorator(
+        color: Colors.cyan,
+        size: const Size(10, 10),
+        activeSize: const Size(22, 10),
+        activeShape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(24)
+        ),
+        activeColor: Colors.red
+      ),
+      curve: Curves.bounceOut,
+    );
+  }
+  PageDecoration getPageDecoration(){
+    return PageDecoration(
+      titleTextStyle: TextStyle(
+        fontSize: 60,
+        fontWeight: FontWeight.bold,
+        color: Colors.white
+      ),
+      bodyTextStyle: TextStyle(
+        fontSize: 22,
+        color: Colors.white70
+      ),
+      imagePadding: EdgeInsets.only(top: 70),
+      pageColor: Color(0xffa40f16)
+    );
+  }
+}
+
+
+
