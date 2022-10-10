621fad3cdccf7f70ee9d69988bd8a1e226cf50a1
doyon <doyon3x3@gmail.com>
2022-10-11 02:23:13 +0900

    음식 장르 페이지 파일

---
+++ Intro/lib/pages/genre_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:introduction_screen/introduction_screen.dart';
+import 'main.dart';
+
+class GenrePage extends StatelessWidget {
+  const GenrePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+
+    );
+  }
+}
