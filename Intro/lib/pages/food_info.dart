11b7e28be252c3bdfcabcd43c828006a7fa29661
doyon <doyon3x3@gmail.com>
2022-11-16 16:48:29 +0900

    add new page : food infos

---
+++ Intro/lib/pages/food_info.dart
@@ -0,0 +1,229 @@
+import 'dart:js';
+
+import 'package:flutter/material.dart';
+class MakeInfoPage extends StatelessWidget {
+  const MakeInfoPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+
+
+class Info_Futomaki extends StatelessWidget {
+  const Info_Futomaki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Gyukatu extends StatelessWidget {
+  const Info_Gyukatu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Gyunikutataki extends StatelessWidget {
+  const Info_Gyunikutataki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Gyutan extends StatelessWidget {
+  const Info_Gyutan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Jingisukan extends StatelessWidget {
+  const Info_Jingisukan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Kamamesi extends StatelessWidget {
+  const Info_Kamamesi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Kare extends StatelessWidget {
+  const Info_Kare({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Katudon extends StatelessWidget {
+  const Info_Katudon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Magurotataki extends StatelessWidget {
+  const Info_Magurotataki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Motunabe extends StatelessWidget {
+  const Info_Motunabe({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Nagasaki extends StatelessWidget {
+  const Info_Nagasaki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Naporitan extends StatelessWidget {
+  const Info_Naporitan({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Nattou extends StatelessWidget {
+  const Info_Nattou({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Omuraisu extends StatelessWidget {
+  const Info_Omuraisu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Otyazuke extends StatelessWidget {
+  const Info_Otyazuke({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Ramen extends StatelessWidget {
+  const Info_Ramen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Sasimi extends StatelessWidget {
+  const Info_Sasimi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Sityu extends StatelessWidget {
+  const Info_Sityu({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Sukiyaki extends StatelessWidget {
+  const Info_Sukiyaki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Supukare extends StatelessWidget {
+  const Info_Supukare({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Tantanmen extends StatelessWidget { //확인 - 라멘에 넣을것인지?
+  const Info_Tantanmen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Tebasaki extends StatelessWidget {
+  const Info_Tebasaki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Tendon extends StatelessWidget {
+  const Info_Tendon({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Tirasizusi extends StatelessWidget {
+  const Info_Tirasizusi({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Unaginokabayaki extends StatelessWidget {
+  const Info_Unaginokabayaki({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Yakisoba extends StatelessWidget {
+  const Info_Yakisoba({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
+class Info_Yakitori extends StatelessWidget {
+  const Info_Yakitori({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
