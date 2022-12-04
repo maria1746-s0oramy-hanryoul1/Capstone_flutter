6662db1729eaf42483932b4717591d22e9a32197
doyon <doyon3x3@gmail.com>
2022-12-05 03:21:22 +0900

    add catalog button of results

--- Intro/lib/pages/main.dart
+++ Intro/lib/pages/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
+// import 'package:intro/pages/foodAppTestPage.dart';
 import 'package:intro/pages/view_all_results.dart';
 import 'package:flutter/services.dart';
 import 'onboarding.dart';
 import 'genre_page.dart';
 import 'test_page.dart';
-
-import 'test_result.dart';
+// import 'test_result.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,8 +17,7 @@ class MyApp extends StatelessWidget{
     //화면 세로 고정
     SystemChrome.setPreferredOrientations([DeviceOrientation.landscapeLeft]);
     return const MaterialApp(
-      home: R_soulmates(),
-     // home: MyPage(),
+      home: OnBoardingPage(),
     );
   }
 }
@@ -31,10 +30,21 @@ class MyPage extends StatelessWidget{
     final ButtonStyle mainButtonStyle = ElevatedButton.styleFrom(
         backgroundColor: const Color(0xffa40f16),
         textStyle: const TextStyle(
-            fontSize: 15, color: Colors.white,
-            fontFamily: 'NotoSans'),
+            fontSize: 16, color: Colors.white,
+            fontFamily: 'NanumSquare',),
         minimumSize: const Size(200, 60)
     );
+    const lineSpace = 20.0;
+
+    // return MaterialApp(
+    //   debugShowCheckedModeBanner: false,
+    //   title: 'testPage',
+    //   theme: ThemeData(
+    //     scaffoldBackgroundColor: Colors.white,
+    //     primarySwatch: Colors.blue,
+    //   ),
+    //   home: const RestaurantPage(),
+    // );
     return Scaffold(
         appBar: AppBar(
         title: const Text('うまうま'),
@@ -44,11 +54,12 @@ class MyPage extends StatelessWidget{
         child:Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Text('우마우마에 오신 것을 환영합니다.',
+            const Text('우마우마에 오신 것을\n환영합니다.',
             style: TextStyle(fontWeight: FontWeight.bold, fontSize: 30,
-            fontFamily: 'NotoSans'),
+            fontFamily: 'NanumSquare', height: 1.5),
+              textAlign: TextAlign.center,
             ),
-            Container( height: 30,),
+            const SizedBox(height: lineSpace),
             ElevatedButton(
                 style: mainButtonStyle,
                 onPressed: (){
@@ -59,9 +70,7 @@ class MyPage extends StatelessWidget{
                 },
                 child: const Text('취향테스트 시작하기',
                 )),
-            Container(
-              height: 15,
-            ),
+            const SizedBox(height: lineSpace),
             ElevatedButton(
                 style: mainButtonStyle,
                 onPressed: () {
@@ -71,7 +80,18 @@ class MyPage extends StatelessWidget{
                   );
                 },
                 child: const Text('장르별 음식 모아보기'),
-            )
+            ),
+            const SizedBox(height: lineSpace),
+            ElevatedButton(
+              style: mainButtonStyle,
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) =>
+                  const ViewAllResults()),
+                );
+              },
+              child: const Text('취향 결과 모아보기'),
+            ),
           ],
          ),
       ),
